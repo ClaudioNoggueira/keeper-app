ca2a4150529dfc8b0077f4a650f3ea8b9f2194e3
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-02-27 10:27:59 -0300

    Use material-ui and conditional render

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import { Fab } from "@mui/material";
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
     const [note, setNote] = useState({
         title: "",
         content: ""
     });
+
+    const [expand, setExpand] = useState(false);
+
+    function handleOnClick() {
+        setExpand(true);
+    }
 
     function handleOnChange(event) {
         const { name, value } = event.target;
@@ -19,22 +28,46 @@ function CreateArea(props) {
 
     return (
         <div>
-            <form onSubmit={(event) => event.preventDefault()}>
-                <input name="title" placeholder="Title" onChange={handleOnChange} value={note.title} />
-                <textarea name="content" placeholder="Take a note..." rows="3" onChange={handleOnChange} value={note.content} />
-                <button
-                    onClick={() => {
-                        props.onAdd(note);
-                        setNote({
-                            title: "",
-                            content: ""
-                        });
-                    }}
+            <form className="create-note" onSubmit={(event) => event.preventDefault()}>
+                {(expand) ?
+                    (
+                        <Zoom in={expand}>
+                            <input
+                                name="title"
+                                placeholder="Title"
+                                onChange={handleOnChange}
+                                value={note.title}
+                            />
+                        </Zoom>
+                    ) : null
+                }
 
-                >Add</button>
+                <textarea
+                    name="content"
+                    placeholder="Take a note..."
+                    rows={expand ? 3 : 1}
+                    onChange={handleOnChange}
+                    onClick={handleOnClick}
+                    value={note.content}
+                />
+
+                <Zoom in={expand}>
+                    <Fab color="primary" aria-label="add"
+                        onClick={() => {
+                            props.onAdd(note);
+                            setNote({
+                                title: "",
+                                content: ""
+                            });
+                        }}
+                    >
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
             </form>
         </div>
     );
 }
+
 
 export default CreateArea;
